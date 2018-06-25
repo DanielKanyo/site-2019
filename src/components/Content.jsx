0d5c2f253058930541c4d8dc510a18644ceef9c0
DanielKanyo <danielkanyog@gmail.com>
2018-06-25 13:08:06 +0200

    Name and job text

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,6 +7,10 @@ class Content extends Component {
       <div className="Content">
         <div className="content-placeholder">
           <div className="content-profile-image"></div>
+          <div className="content-name">
+            <h3>Daniel Kanyo</h3>
+            <h6>DEVELOPER</h6>
+          </div>
         </div>
       </div>
     );
